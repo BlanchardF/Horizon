7701a24141d7de3bcc6ba17a403288ba2dff155f
Florian Blanchard <fblanchard@univ-lyon1.fr>
2024-10-04 16:35:28 +0200

    new rules

--- script/TH_horizon/choose_predict.py
+++ script/TH_horizon/choose_predict.py
@@ -1,4 +1,4 @@
->#!/usr/bin/python3
+#!/usr/bin/python3
 
 #Ligne de commande : 
 ##python3 Choose_predict.py -i input_file.gff -o output_file.gff
@@ -20,13 +20,13 @@ def choose_predict(input_file, output_file):
 
     for l in in_file: 
         line=l.strip().split("\t")
-        Size_a=int(line[4])-int(line[3])
-        Size_b=int(line[14])-int(line[13])
+        Size_a=int(line[2])-int(line[1])
+        Size_b=int(line[6])-int(line[5])
 
         if Size_a >= Size_b :
-            out.write("{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\n".format(str(line[0]),str(line[1]),str(line[2]),str(line[3]),str(line[4]),str(line[5]),str(line[6]),str(line[7]),str(line[8]),str(line[9])))
+            out.write("{}\t{}\t{}\t{}\n".format(str(line[0]),str(line[1]),str(line[2]),str(line[3])))
         else : 
-            out.write("{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\n".format(str(line[10]),str(line[11]),str(line[12]),str(line[13]),str(line[14]),str(line[15]),str(line[16]),str(line[17]),str(line[18]),str(line[19])))
+            out.write("{}\t{}\t{}\t{}\n".format(str(line[4]),str(line[5]),str(line[6]),str(line[7])))
 
 
 def main(): 
